b44a3675d67cdf775f21c713189af33c4ca2b92b
Vladislav Tcendrovskii <ccvs@mail.ru>
2013-01-26 15:32:41 +0000

    compiler script uses -lSDL_draw-1.2

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-clang++ -lSDL -lSDL_ttf -lSDL_image -I/usr/include/SDL -o rtype2 main.cc libSDL_draw.a -lpng
+$CXX -lSDL -lSDL_ttf -lSDL_image -I/usr/include/SDL -o rtype2 main.cc -lSDL_draw-1.2 -lpng
+
